67b76297dd155fff70d093aa89f4c2d5556fe349
AmirAi <110692865+AmirIXI@users.noreply.github.com>
2024-07-05 15:06:19 +0330

    Add files via upload

---
+++ v1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string basePath = "C:\\Users\\amira\\OneDrive\\Desktop\\c#";
+        while (true)
+        {
+            string folderName = Path.GetRandomFileName();
+            string folderPath = Path.Combine(basePath, folderName);
+            Directory.CreateDirectory(folderPath);
+            Console.WriteLine("Created folder: " + folderPath);
+        }
+    }
+}
